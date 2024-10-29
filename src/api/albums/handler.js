38f11d47924e098467886cec55b997f581d7265c
Tods <algy25ng@gmail.com>
2024-10-29 09:14:28 +0800

    inisialisasi awal

---
+++ src/api/albums/handler.js
@@ -0,0 +1,62 @@
+const autoBind = require('auto-bind');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { name, year } = request.payload;
+
+    const albumId = this._service.addAlbum({ name, year });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  getAlbumByIdHandler(request) {
+    const { id } = request.params;
+    const album = this._service.getAlbumById(id);
+    return {
+      status: 'success',
+      data: {
+        album,
+      },
+    };
+  }
+
+  putAlbumByIdHandler(request) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+
+    this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui',
+    };
+  }
+
+  deleteAlbumByIdHandler(request) {
+    const { id } = request.params;
+    this._service.deleteAlbumById(id);
+
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
