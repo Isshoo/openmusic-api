2d1fc592ca3bfcb76f7c6b6b17d0b46cb47cc3e4
Tods <algy25ng@gmail.com>
2024-10-30 04:21:16 +0800

    update tabel songs

--- migrations/1730163178733_create-tabel-songs.js
+++ migrations/1730163178733_create-tabel-songs.js
@@ -30,7 +30,7 @@ exports.up = (pgm) => {
     },
   });
 
-  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY("album_id") REFERENCES albums(id) ON DELETE CASCADE');
+  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
