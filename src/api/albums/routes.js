939edc54a0d003a878b7203ff6c5072ee97a0dfe
Tods <algy25ng@gmail.com>
2024-10-31 08:21:39 +0800

    new routes untuk album dan export serta tabel likes_album

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -19,6 +19,26 @@ const routes = (handler) => [
     path: '/albums/{id}',
     handler: handler.deleteAlbumByIdHandler,
   },
+  {
+    method: 'POST',
+    path: '/albums/{id}/covers',
+    handler: handler.postAlbumCoverByIdHandler,
+  },
+  {
+    method: 'POST',
+    path: '/albums/{id}/likes',
+    handler: handler.postAlbumLikesByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}/likes',
+    handler: handler.deleteAlbumLikesByIdHandler,
+  },
+  {
+    method: 'GET',
+    path: '/albums/{id}/likes',
+    handler: handler.getAlbumLikesByIdHandler,
+  },
 ];
 
 module.exports = routes;
